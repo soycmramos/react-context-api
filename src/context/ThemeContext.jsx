68165da6ecbc8b5c702662b3349cf30d789e7de9
soycmramos <carlosmarioramos34@gmail.com>
2024-05-25 17:03:21 -0500

    Set dark mode

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,12 +1,54 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+
+const setStorageTheme = theme => {
+  window.localStorage.setItem('theme', theme)
+}
+const getStorageTheme = () => {
+  return window.localStorage.getItem('theme')
+}
+
+const getPreferColorScheme = () => {
+  if (!getStorageTheme()) {
+    if (matchMedia('(prefers-color-scheme: dark)').matches) {
+      setStorageTheme('dark')
+      return
+    }
+
+    if (matchMedia('(prefers-color-scheme: light)').matches) {
+      setStorageTheme('light')
+      return
+    }
+  }
+
+  return getStorageTheme()
+}
 
 export const ThemeContext = createContext()
-
 export default function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState(getPreferColorScheme())
 
-  const [theme, setTheme] = useState('light')
+  const handleTheme = () => {
+    if (getStorageTheme() === 'dark') {
+      setStorageTheme('light')
+    } else if (getStorageTheme() === 'light') {
+      setStorageTheme('dark')
+    }
 
-  const handleTheme = () => setTheme(currentTheme => currentTheme === 'light' ? 'dark': 'light')
+    setTheme(getStorageTheme())
+  }
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document
+        .querySelector('body')
+        .classList.add('dark')
+    } else if (theme === 'light') {
+      document
+        .querySelector('body')
+        .classList.remove('dark')
+    }
+  }, [theme])
+
 
   return (
     <ThemeContext.Provider value={{ theme, handleTheme }}>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,13 +3,10 @@ import Header from './../components/Header'
 import Footer from './../components/Footer'
 
 export default function Layout({ children }) {
-
-  const { theme } = useThemeContext()
-
   return (
     <>
       <Header />
-      <main className={`min-h-screen grid place-items-center ${ theme === 'dark' ? 'bg-gray-900' : 'bg-slate-100' }`}>
+      <main className='min-h-screen grid place-items-center bg-slate-100 dark:bg-neutral-900'>
         {children}
       </main>
       <Footer />
